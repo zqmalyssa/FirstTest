a18da507bb86113a8a475015fa47d7b37101f9df
zqmalyssa <yzzqm@hotmail.com>
2017-01-10 15:38:07 +0800

    first commit

---
+++ com.qiming/src/main/java/First/com/qiming/App.java
@@ -0,0 +1,20 @@
+package First.com.qiming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void test(){
+    	
+    	System.out.println("test ok");
+    	
+    }
+    
+}
